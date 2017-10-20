a8548679977b1bf3a205e4d808b113aa128f649c
gingyneil <neil.reading.nr@gmail.com>
2017-10-20 00:03:36 -0400

    Exercise 3 Complete

--- app/src/main/java/com/example/user1/neilreading_300793474_lab3/Ex3Activity.java
+++ app/src/main/java/com/example/user1/neilreading_300793474_lab3/Ex3Activity.java
@@ -1,13 +1,41 @@
 package com.example.user1.neilreading_300793474_lab3;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 public class Ex3Activity extends AppCompatActivity {
 
+
+     ImageView  moonImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ex3);
+    }
+
+
+
+    public void startBtnClick(View view) {
+
+        // animate the image
+        moonImage = (ImageView) findViewById(R.id.moonImageView);
+
+        // load the circleme.XML animation
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.circleme);
+
+
+
+
+        moonImage.startAnimation(animation);
+
+
+    }
+
+        public void stopBtnClick(View view) {
+        moonImage.clearAnimation();
     }
 }
